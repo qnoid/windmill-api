1a5202e36b64aa53e9b027fa6afddcb0741a2a5a
Markos Charatzas <markos@qnoid.com>
2016-05-22 21:04:39 +0100

    fixed persistence.xml to use the renamed postgres properties now using postgres.url instead of ip:port since one property can capture both updated deploy to AWS script to use the ROOT naming which the artifact has been changed to

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Copying windmill war to AWS, you need to have the your war deployed with AWS properties";
-scp -i ~/.ssh/windmill.pem ./target/windmill.war ubuntu@api.windmill.io:~/.linuxbrew/opt/wildfly-as/libexec/standalone/deployments/windmill.war
+scp -i ~/.ssh/windmill.pem $1 ubuntu@api.windmill.io:~/.linuxbrew/opt/wildfly-as/libexec/standalone/deployments/ROOT.war
